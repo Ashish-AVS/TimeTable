120d95595dd7d859799399dc083d0c24ee63385f
Ashish-AVS <65858601+Ashish-AVS@users.noreply.github.com>
2020-08-20 11:06:52 +0530

    Update MeetLinks.js

--- src/MeetLinks/MeetLinks.js
+++ src/MeetLinks/MeetLinks.js
@@ -1,6 +1,6 @@
 const meetLinks={
-	PHYF212L:"https://meet.google.com/wrp-yxye-smo",
-	PHYF212T:"https://www.google.com",
+	PHYF212L:"https://meet.google.com/epp-ozhv-ozf",
+	PHYF212T:"https://meet.google.com/epp-ozhv-ozf",
 
 	CHEMF211L:"https://meet.google.com/ecp-vymi-hsr",
 	CHEMF211T:"https://meet.google.com/ecp-vymi-hsr",
