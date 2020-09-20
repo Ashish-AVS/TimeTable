e00d692fcc118a7378c1563072ac62c3ecd9976d
Ashish-AVS <avsashish9@gmail.com>
2020-09-20 19:15:31 +0530

    Improvements Required

--- src/App.js
+++ src/App.js
@@ -11,7 +11,16 @@ class App extends Component {
     return(
       <div>
         <DropDown />
+        <h1 id="head">TIME TABLE</h1>
         <Table display="Light"/>
+        <ul style={{color:"aliceblue", textAlign: "center", padding: "10px",marginTop:"50px", backgroundColor:"#343434"}}>
+          <h3>IMPROVEMENTS PENDING..</h3>
+          <li>NodeJs</li>
+          <li>MongoDB</li>
+          <li>Advanced CSS AND SASS</li>
+          <li>FireBase</li>
+          <li>NOVALDOCOD</li>
+        </ul>
       </div>
     );
   }
