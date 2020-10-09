003a09b51845f77781f50c375c3be2a5ac91876a
ThatAwes0meGuy <62514125+ThatAwes0meGuy@users.noreply.github.com>
2020-10-09 14:44:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 import Table from './Table/Table';
 import DropDown from './DropDown/DropDown';
+import Switch from "./Switch/Switch";
 
 class App extends Component {
   state = {
     display: false
   }
+
   render() {
     
     return(
       <div>
-        <DropDown />
-        <h1 id="head">TIME TABLE</h1>
+        <div className="switch"><Switch /></div>
+        <h1 id="head">TIME TABLE</h1> 
         <Table display="Light"/>
-        <ul style={{color:"aliceblue", textAlign: "center", padding: "10px",marginTop:"50px", backgroundColor:"#343434"}}>
-          <h3>IMPROVEMENTS PENDING..</h3>
-          <li>NodeJs</li>
-          <li>MongoDB</li>
-          <li>Advanced CSS AND SASS</li>
-          <li>FireBase</li>
-          <li>NOVALDOCOD</li>
-        </ul>
       </div>
     );
   }
